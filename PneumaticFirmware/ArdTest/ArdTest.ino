211f00af293f2107fc6c36c5e149a9fb063bd665
rjweld21 <rj.weld.021@gmail.com>
2018-10-04 08:28:38 -0400

    adding more serial comm tests between arduino and python, also created class for handling arduino serial comm

---
+++ PneumaticFirmware/ArdTest/ArdTest.ino
@@ -0,0 +1,74 @@
+#include <string.h>
+#include <stdio.h>
+
+String data;
+char cData;
+int mode=0; //0 - constant, 1 - pulse, 2 - ramp
+int SDAC, EDAC, MS;
+int charlen;
+const char delim[2] = ",";
+char *token;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("HELLO");
+}
+
+void sWrite(String header, char *s)
+{
+  Serial.println(header);
+  Serial.println(s);
+}
+
+void loop() {
+  
+  // Enter loop to read serial buffer
+  while (Serial.available() > 0)
+  {
+    // Read line of serial data from computer
+    data = Serial.readStringUntil('\n');
+
+    // Rewrite data back to buffer
+    //Serial.println(data);
+
+    // If on last pass for reading...
+    if (Serial.available() == 0)
+    {
+      //charlen = (int)strlen(data)
+      data.toCharArray(cData, data.length());
+
+      sWrite("CHAR DATA", cData);
+      Serial.println("DATA");
+      Serial.println(data);
+      Serial.println(data.length());
+      
+      // Confirm data has been sent by GUI python program
+      token = strtok(cData, delim);
+      Serial.println("FIRST");
+      Serial.println(token);
+      
+      if (~(token == "PYSERIAL")) // Otherwise, exit
+      {
+        continue;
+      }
+      
+      token = strtok(NULL, delim);
+      Serial.println("SECOND");
+      Serial.println(token);
+      
+      if (token == "constant")
+      {
+        Serial.println("constant");
+      }
+      else if (token == "pulse")
+      {
+        Serial.println("pulse");
+      }
+      else
+      {
+        Serial.println("ramp");
+      }
+    }
+  }
+}
--- ProcessingGUI/control/serialTest.py
+++ ProcessingGUI/control/serialTest.py
@@ -1,27 +1,92 @@
 import serial
+from time import sleep
 
-port1 = '\\\\.\\CNCA0'
-ser1 = serial.Serial(port1, 38400, timeout=0)
+class SerialArduino(object):
+    def __init__(self, port='COM7', br=9600, timeout=0):
+        self.SA = serial.Serial(port, br, timeout=timeout)
+        self.old = b''
+        self.data = b''
+        
+    def write_buffer(self, data):
+        if type(data) == str:
+            data = data.encode('utf-8')
+        elif type(data) == int:
+            data = str(data).encode('utf-8')
+            
+        if not type(data) == bytes:
+            print('Data must be int, string or bytes!')
+            print('No data written to serial.')
+            return
+            
+        self.SA.write(data)
 
-port2 = '\\\\.\\CNCB0'
-ser2 = serial.Serial(port2, 38400, timeout=0)
+    def read_buffer(self, byteLen=0, parse=True):
+        if not byteLen:
+            byteLen = self.bufferSize()
+            
+        self.data = self.old + self.SA.read(byteLen)
+        
+        if parse and len(self.data) > 0:
+            self.parseData()
+            
+    def parseData(self):
+        if self.data[-1] == '\n':
+            self.old = b''
+            return
+            
+        parsed = self.data.decode('utf-8').split('\n')
+        self.data = '\n'.join(parsed[:-1]).encode('utf-8')
+        self.old = parsed[-1].encode('utf-8')
+        
+    def bufferSize(self):
+        return self.SA.in_waiting
+        
+def connectVirtualComs(ports=['\\\\.\\CNCA0',
+                                '\\\\.\\CNCB0'],
+                        br=38400, timeout=0):
+    ser = []
+    
+    for p in ports:
+        ser.append(SerialArduino(p, br, timeout))
+        
+    return ser
+    
+def parseData(data):
+    if data[-1] == '\n':
+        return data, b''
+        
+    parsed = data.decode('utf-8').split('\n')
+    data = '\n'.join(parsed[:-1])
+    oldData = parsed[-1].encode('utf-8')
+    
+    return data, oldData
+    
+    
+if __name__ == '__main__':
+    try:
+        print('Trying arduino...')
+        port2 = 'COM7'
+        ser2 = SerialArduino()
+        print('Connected!')
+    except Exception as e:
+        print('\n\n' + '!'*40 + '\n\nFAILED CONNECTING TO ARDUINO\n')
+        print('!'*40, '\n\n')
+        print('\nError:', e, '\n\n')
+        [ser1, ser2] = connectVirtualComs()
 
-ser1.write(b'Hello\nMy Name Is\nRJ\n')
-ser1.write(b'')
-ser1.write(b'')
+    while True:
+        print('='*40)
+        print('BYTES IN BUFFER:',ser2.bufferSize())
+        ser2.read_buffer()
+        if len(ser2.data)-len(ser2.old) > 0:
+            print('DATA FROM BUFFER:', ser2.data)
+            print('DATA DECODED:', ser2.data.decode('utf-8'))
+            print('='*40, '\n')
+            ser2.write_buffer(b'PYSERIAL,constant,0\n')
+        else:
+            print('No more data... Sleeping...')
+            print('='*40, '\n')
+            sleep(2)
 
-while True:
-    print('='*40)
-    print('BYTES IN BUFFER:',ser2.in_waiting)
-    data = ser2.read(ser2.in_waiting)
-    if len(data) > 0:
-        print('DATA FROM BUFFER:', data)
-        print('DATA DECODED:', data.decode('utf-8'))
-        print('='*40, '\n')
-    else:
-        print('No more data... Exiting...')
-        print('='*40, '\n')
-        break
-
-ser1.close()
-ser2.close()
+    ser1.close()
+    ser2.close()
